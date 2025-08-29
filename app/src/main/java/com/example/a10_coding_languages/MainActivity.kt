71b0b7b4575e855f10e1befc7ec67a47e2e05464
mataverrick <mataveraerick@gmail.com>
2025-08-29 13:53:25 -0600

    feature: card animation is ready

--- app/src/main/java/com/example/a10_coding_languages/MainActivity.kt
+++ app/src/main/java/com/example/a10_coding_languages/MainActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -21,6 +27,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +40,10 @@ import androidx.compose.ui.unit.dp
 import com.example.a10_coding_languages.data.LanguageList
 import com.example.a10_coding_languages.model.Language
 import com.example.a10_coding_languages.ui.theme._10_coding_languagesTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,11 +80,14 @@ fun MainApp(modifier: Modifier = Modifier) {
 
 @Composable
 fun CardLanguage(language: Language, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Card(
+            onClick = { expanded = !expanded },
             modifier = Modifier
                 .fillMaxWidth(0.8f)
                 .clip(RoundedCornerShape(bottomStart = 30.dp, topEnd = 30.dp))
@@ -81,7 +95,13 @@ fun CardLanguage(language: Language, modifier: Modifier = Modifier) {
             Column {
                 LanguageTitles(Modifier.padding(8.dp))
                 LanguageImage(Modifier.fillMaxWidth())
-                LanguageDescription(Modifier.padding(8.dp))
+                AnimatedVisibility(
+                    visible = expanded,
+                    enter = expandVertically(expandFrom = Alignment.Top),
+                    exit = shrinkVertically(shrinkTowards = Alignment.Top)
+                ) {
+                    LanguageDescription(Modifier.padding(8.dp))
+                }
             }
         }
     }
